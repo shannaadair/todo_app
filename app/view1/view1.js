647472632147b0ac6539b71c1e3c57d5ac12a001
doug chamberlain <dchamberlain@identifi.net>
2016-03-21 15:31:29 -0400

    this patch makes a list and strikes through completed items.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,16 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl',
+    controllerAs: 'vm'
   });
 }])
 
 .controller('View1Ctrl', [function() {
-
+  var vm = this;
+  vm.todos = [{name:"make site", done: true}, {name: "profit"}];
+  vm.createNewToDo = function(name){
+   vm.todos.push({name: name});
+    vm.newToDo = "";
+  }
 }]);
